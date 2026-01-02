b00cf40ac73dbdcc3a27dbe78f56768d288a023d
kiyo4810 <kiyo4810@gmail.com>
2026-01-02 09:26:19 +0900

    インクリメントの練習修正

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const cats = ["ごまちゃん", "パンくん", "レオくん"];
 // i というカウンターを作って、0から順番に1ずつ増やしていく
 for (let i = 0; i < cats.length; i++) {
     console.log( (i + 1) + "番目の猫は" + cats[i] );
-    document.getElementById("cat" + (i +1)).innerText;
+    document.getElementById("cat" + (i +1)).innerText = cats[i];
 }
